3045b8bbdb1676e01f1693214f4c1babcdbf6935
jonake15 <113668172+jonake15@users.noreply.github.com>
2024-01-22 16:42:29 +0300

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,30 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const navbar = useRef();
   const ul = useRef();
+  const openButton = useRef();
+  const closeButton = useRef();
 
   const handleClick = () => {
     navbar.current.classList.toggle("block");
     ul.current.classList.toggle("ul-block");
+    openButton.current.classList.toggle("hide");
+    closeButton.current.classList.toggle("show");
     // ul.current.style.visibility = "visible";
     // ul.current.style.display = "block";
   };
   return (
     <div ref={navbar} className="navbar">
       <button className="btn" onClick={handleClick}>
-        Click
+        <i
+          ref={openButton}
+          style={{ color: "white", fontSize: "1.5rem" }}
+          class="bi bi-list"
+        ></i>
+        <i
+          ref={closeButton}
+          style={{ color: "white", fontSize: "1.5rem" }}
+          class="bi bi-x"
+        ></i>
       </button>
 
       <ul ref={ul}>
